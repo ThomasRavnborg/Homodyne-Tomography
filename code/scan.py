dab7c40408e33eb1434b2457d7554a6db40d2276
Jan Scarabelli <jan.scarabelli@gmail.com>
2025-08-15 11:51:29 +0200

    get timestamps correctly

--- code/scan.py
+++ code/scan.py
@@ -10,16 +10,43 @@ Created on Wed Aug 14 2025
 import numpy as np
 import json
 import os
+from natsort import natsorted
 
 from pathlib import Path
 
+
+#%% define functions
+def fs(t, t0):
+    """
+    Temporal mode function
+    """
+    f = 9*1e6 # Hz
+    gamma = f*2*np.pi
+    return np.sqrt(gamma)*np.exp(-gamma*np.abs(t-t0))
+
+
+def vac_normalise(trace, vac_mean, vac_std):
+    """
+    Normalise trace wrt vaccum data
+    """
+    pass
 #%% read data
-parent = Path(os.path.dirname(os.getcwd()))
+parent = Path.cwd()  # Get the parent directory (repo root directory)
 date = "091027"
-state = "cat1"
+#state = "cat1"
 
-data_path = parent / "data" / "processed_data" / date / (state + '.npy')
+data_folder = parent / "data" / "processed_data"
 
-state_dirs = [d.name for d in natsorted((data_folder / date).iterdir()) if d.is_dir() and \
-                  ('cat' in d.name or 'tora' in d.name)]
-for state in 
+states_list = [d.name.split('.')[0] for d in natsorted((data_folder / date).iterdir()) if 
+               'dts' not in d.name]
+
+state_ts = {}
+for state in states_list:
+    dt = json.load(open(data_folder / date / 'dts.json'))[state]
+    data = np.load(data_folder / date / (state+'.npy'))
+
+    #%% Get timestamp
+    data_angles = np.delete(data, 12, axis=0)  # remove vacuum
+    state_vars = data_angles.var(axis=1)  # get variance of 30000 points
+    ts = np.argmax(state_vars)  # get timestamp of maximum variance
+    state_ts[state] = np.round(ts.mean())  # select avg time
