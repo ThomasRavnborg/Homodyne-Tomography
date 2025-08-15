f3afa531046f6aac6e90720386fee2003a805228
ThomasRavnborg <s214434@student.dtu.dk>
2025-08-15 11:35:42 +0200

    Modified files

--- code/calculate_quadratures.py
+++ code/calculate_quadratures.py
@@ -1,21 +1,9 @@
 import numpy as np
-import os
-from pathlib import Path
-import json
 
 def fs(t, t0):
     f = 9*1e6 # Hz
     gamma = 2*np.pi*f
     return np.sqrt(gamma)*np.exp(-gamma*np.abs(t-t0))
-
-# Loading data
-parent = Path(os.path.dirname(os.getcwd()))
-date = "091027"
-state = "cat2"
-data_path = parent / "data" / "processed_data" / date
-data = np.load(data_path / (state + '.npy'))
-
-dt = json.load(open(data_path / 'dts.json'))[state]
 
 def calculate_quadratures(data, dt):
 
@@ -33,17 +21,11 @@ def calculate_quadratures(data, dt):
     vacuum_mean = np.mean(vacuum_quadratures)
     vacuum_std = np.std(vacuum_quadratures)
 
-
-    x_values = np.array([])
-    theta_values = np.array([])
+    x_values = np.empty((12, 10000))
 
     for i in range(12):
         state = data[i,0,:,:]
         # Project each trace onto the temporal mode
         quadratures = state @ temporal_mode * dt
-        # Normalize quadratures to vacuum
         quadratures = (quadratures - vacuum_mean) /(np.sqrt(2)*vacuum_std)
-        # Append quadrature values and angles
-        x_values = np.append(x_values, quadratures)
-        theta_values = np.append(theta_values, np.full_like(quadratures, 15*i))
-        theta_values = np.deg2rad(theta_values)
+        x_values[i] = quadratures
